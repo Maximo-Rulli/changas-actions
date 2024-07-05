e5cafb558e4257441344beffc3c4437d0e31ea83
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-07-04 23:26:58 -0300

    implemented action to only fetch data to login user and secure store

--- login.js
+++ login.js
@@ -1,35 +1,28 @@
-import supabase from '../libs/supabase/server';
-import bcrypt from '../bcryptSetup';
-import messages from '../utils/messages';
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from 'expo-secure-store'
+import supabase from '../libs/supabase/server'
+import messages from '../utils/messages'
 
-export default async function AuthLogin(email, password){
-    const { data, error} = await supabase.from('users').select('*').eq('email', email).single();
+export default async function AuthLogin(email){
+    const { data, error} = await supabase.from('users').select('*').eq('email', email).single()
     
     if (error) {
-        throw new Error(messages.error.error);
+        throw new Error(messages.error.error)
     }
     if (!data) {
-        throw new Error(messages.error.user_not_found);
+        throw new Error(messages.error.user_not_found)
     }
-    const validPassword = await bcrypt.compare(password, data.password);
+    
+    const { data: userdata, error: usererror} = await supabase.from('users_data').select('name, surname').eq('email', email).single()
+    const username = userdata.name + ' ' + userdata.surname
 
-    if (validPassword) {
-        const { data: userdata, error: usererror} = await supabase.from('users_data').select('name, surname').eq('email', email).single();
-        const username = userdata.name + ' ' + userdata.surname;
-
-        if (!usererror){
-            // Save the data to secure store
-            await SecureStore.setItemAsync('id_user', data.id_user);
-            await SecureStore.setItemAsync('username', username);
-            return [data.id_user, username];
-        }
-        else {
-            throw new Error(messages.error.error);
-        }
+    if (!usererror){
+        // Save the data to secure store
+        await SecureStore.setItemAsync('id_user', data.id_user)
+        await SecureStore.setItemAsync('username', username)
+        return [data.id_user, username]
     }
     else {
-        throw new Error(messages.error.incorrect_password);
+        throw new Error(messages.error.error)
     }
 
 }
