d42e2c09e9fd329d85af0799908449883593f6f0
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-07-07 22:34:38 -0300

    updated function to new schema

--- getMessages.js
+++ getMessages.js
@@ -1,6 +1,7 @@
 import supabase from '../libs/supabase/server'
 
 export async function getMessages (IdChat) {
-  const { data: chat } = await supabase.from('chats').select('content').eq('id_chat', IdChat).single()
+  const { data: chat } = await supabase.from('messages').select('content, user_1')
+    .eq('id_chat', IdChat).order('time', { ascending: false })
   return chat
 }
