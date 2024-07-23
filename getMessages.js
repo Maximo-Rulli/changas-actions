2ad02d58b4cfb7ba8d5b64b74406f0ba2d9270b5
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-07-23 14:43:11 -0300

    updated function to retrieve time

--- getMessages.js
+++ getMessages.js
@@ -1,7 +1,7 @@
 import supabase from '../libs/supabase/server'
 
 export async function getMessages (IdChat) {
-  const { data: chat } = await supabase.from('messages').select('content, user_1')
+  const { data: chat } = await supabase.from('messages').select('content, user_1, time')
     .eq('id_chat', IdChat).order('time', { ascending: false })
   return chat
 }
