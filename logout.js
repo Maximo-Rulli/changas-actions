0f0e2904c99c39a1d68cf348b022c5f60766aad6
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-07-22 21:46:36 -0300

    improved functio

--- logout.js
+++ logout.js
@@ -5,7 +5,7 @@ export default async function Logout({navigation}){
     await SecureStore.deleteItemAsync('username');
     // Reset the stack as the user just logged out and cannot go back
     navigation.reset({
-        index: 0,
-        routes: [{ name: 'Login' }],
-      });
+      index: 0,
+      routes: [{ name: 'Login' }],
+    })
 }
