e00e4d936d523a25ca785fc938c9b08c30004ba8
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-08-08 18:08:48 -0300

    updated actions

--- getWorkers.js
+++
@@ -1,7 +0,0 @@
-import supabase from '../libs/supabase/server'
-
-export async function getWorkers (category, columns) {
-  const { data } = await supabase.from('workers').select(columns).eq('category', category)
-
-  return data
-}
