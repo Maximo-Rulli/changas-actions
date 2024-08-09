f039a807aa8e36a0420c5ee9df2148383e9fd619
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-08-09 00:34:44 -0300

    delete unnecessary function

--- getWorker.js
+++
@@ -1,7 +0,0 @@
-import supabase from '../libs/supabase/server'
-
-export async function getWorker (IdUser, columns) {
-  const { data } = await supabase.from('workers').select(columns).eq('id_user', IdUser).single()
-
-  return data
-}
