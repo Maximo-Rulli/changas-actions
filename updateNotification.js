0028856d3338cd741942de7e7703ece60ad0ca8c
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-08-18 16:14:06 -0300

    fixed import

--- updateNotification.js
+++ updateNotification.js
@@ -1,5 +1,5 @@
 import supabase from '../libs/supabase/server'
-import { getPermissionsAsync } from 'expo-notifications'
+import { getPermissionsAsync, getExpoPushTokenAsync } from 'expo-notifications'
 import * as SecureStore from 'expo-secure-store'
 import Constants from 'expo-constants'
 
@@ -14,7 +14,7 @@ export async function updateNotification (IdUser) {
       alert('Hubo un error al buscar el ID del proyecto, las notificaciones no funcionarán. Contactese con soporte.')
     }
     else {
-      const newToken = (await Notifications.getExpoPushTokenAsync({projectId,})).data
+      const newToken = (await getExpoPushTokenAsync({projectId,})).data
       await supabase.from('users_data').update({'push_token': newToken}).eq('id_user', IdUser)
       await SecureStore.setItemAsync('push_token', newToken)
     }
