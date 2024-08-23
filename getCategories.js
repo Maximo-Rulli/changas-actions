896a324868b0ad8f3af8d4e6a5c6a5c608b6edc8
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-08-23 19:56:05 -0300

    implemented ascending loading of jobs

--- getCategories.js
+++ getCategories.js
@@ -1,7 +1,7 @@
 import supabase from '../libs/supabase/server'
 
 export async function getCategories (columns) {
-  const { data } = await supabase.from('categories').select(columns)
+  const { data } = await supabase.from('categories').select(columns).order('id', { ascending: true })
 
   return data
 }
