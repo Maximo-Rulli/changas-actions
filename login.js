49c4cba5361c11c60e9954ed60d95d27e43b2c47
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-09-02 21:49:42 -0300

    implemented login with username

--- login.js
+++ login.js
@@ -12,13 +12,12 @@ export default async function AuthLogin(email){
         throw new Error(messages.error.user_not_found)
     }
     
-    const { data: userdata, error: usererror} = await supabase.from('users_data').select('name, surname').ilike('email', email).single()
-    const username = userdata.name + ' ' + userdata.surname
+    const { data: userdata, error: usererror} = await supabase.from('users_data').select('username').ilike('email', email).single()
 
     if (!usererror){
         // Save the data to secure store
         await SecureStore.setItemAsync('id_user', data.id_user)
-        await SecureStore.setItemAsync('username', username)
+        await SecureStore.setItemAsync('username', userdata.username)
         return [data.id_user, username]
     }
     else {
