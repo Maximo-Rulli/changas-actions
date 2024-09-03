6f42effe43da60c99b979f9922c131a95b526793
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-09-02 23:23:50 -0300

    fixed function

--- login.js
+++ login.js
@@ -12,13 +12,13 @@ export default async function AuthLogin(email){
         throw new Error(messages.error.user_not_found)
     }
     
-    const { data: userdata, error: usererror} = await supabase.from('users_data').select('username').ilike('email', email).single()
+    const { data: userdata, error: usererror} = await supabase.from('users_data').select('username, status').ilike('email', email).single()
 
     if (!usererror){
         // Save the data to secure store
         await SecureStore.setItemAsync('id_user', data.id_user)
         await SecureStore.setItemAsync('username', userdata.username)
-        return [data.id_user, userdata.username]
+        return [data.id_user, userdata.username, userdata.status]
     }
     else {
         throw new Error(messages.error.error)
