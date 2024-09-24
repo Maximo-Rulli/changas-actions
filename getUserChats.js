aa395c937ce1b94c0ea3072edd781d346ab16547
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-09-23 23:12:36 -0300

    reimplemented function to use inner joins

--- getUserChats.js
+++ getUserChats.js
@@ -1,8 +1,10 @@
 import supabase from '../libs/supabase/server'
 
 export async function getUserChats (IdUser) {
-  const columns = 'id_chat, id_user1, id_user2, username_1, username_2, read_user_1, read_user_2, last_message, blocked'
-  const { data: chats } = await supabase.from('chats').select(columns)
+  const columns = 'id_chat, id_user1, id_user2, username_1, username_2, read_user_1, read_user_2, last_message, blocked, '
+  const innerJoin = 'users_data1: users_data!id_user1(username, status), users_data2: users_data!id_user2(username, status)'
+  const { data: chats } = await supabase.from('chats').select(columns+innerJoin)
     .or(`id_user1.eq.${IdUser}, id_user2.eq.${IdUser}`).order('last_message', { ascending: false })
+  
   return chats
 }
